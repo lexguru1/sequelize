f507e3bddcf8a7e576f7594f4f5b6d6826a1e41b
lexguru1 <alexghiurutan@gmail.com>
2022-03-14 13:56:59 +0100

    Final commit, data was imported

---
+++ seeders/20220314113803-some-users.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+   
+     await queryInterface.bulkInsert(
+       'users',
+        [
+          {
+            name: "Leo Messi",
+            email: "leo@messi.com",
+            phone: 1234567,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            name: "Dan Abramov",
+            email: "dan@redux.com",
+            phone: 1234567,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+      ], 
+      {}
+      );
+    
+  },
+
+  async down (queryInterface, Sequelize) {
+   
+     await queryInterface.bulkDelete('users', null, {});
+     
+  }
+};
